e819634df2356ecf126e231102e3644b467e9d14
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-01 10:46:19 +0200

    1st commit

---
+++ qex1.php
@@ -0,0 +1,34 @@
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+-->
+<?php include 'quang22000Layout/header.php' ?>
+
+<h1>3.1. Write a simple PHP script to print your information (Name and your groupid).</h1>
+<?php
+echo "Quang Luong <br>";
+echo "CA2022<br>";
+?>
+<h1>3.2 Write PHP code to display the following message.</h1>
+<?php
+echo "3.2.1 Hello world! My name is \"Quang\"";
+?>
+<h1>3.3. Current Date</h1>
+<?php
+echo date("d.m.Y")
+?>
+<h1>3.4. to be continued</h1>
+
+<!--
+</body>
+</html>
+-->
+
+<?php include 'quang22000Layout/footer.php' ?>
