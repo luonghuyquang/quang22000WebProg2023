847b68c86788b8d249b0524e6583888bbbab2712
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-01 11:26:30 +0200

    in class exercise 1

--- qex1.php
+++ qex1.php
@@ -11,20 +11,52 @@
 -->
 <?php include 'quang22000Layout/header.php' ?>
 
-<h1>3.1. Write a simple PHP script to print your information (Name and your groupid).</h1>
+<h3>3.1. Write a simple PHP script to print your information (Name and your groupid).</h3>
 <?php
 echo "Quang Luong <br>";
 echo "CA2022<br>";
 ?>
-<h1>3.2 Write PHP code to display the following message.</h1>
+<h3>3.2 Write PHP code to display the following message.</h3>
 <?php
 echo "3.2.1 Hello world! My name is \"Quang\"";
 ?>
-<h1>3.3. Current Date</h1>
+
+<h3>3.3. Current Date</h3>
 <?php
 echo date("d.m.Y")
 ?>
-<h1>3.4. to be continued</h1>
+
+<h3>3.4. Variable in php</h3>
+<?php
+$title1 = "PHP is interesting";
+echo "<h1>".$title1 . "</h1>";
+?>
+
+<h3>3.5. Table & Variables</h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 3;
+echo "
+<table>
+<tr>
+<th> S.N </th> <th> Name </th><th> Grade </th>
+</tr>
+<tr>
+<td> 1 </td> <td> Pekka </td> <td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td> <td> Johanna </td> <td> $g2 </td>
+</tr>
+<tr>
+<td> 3 </td> <td> John </td> <td> $g3 </td>
+</tr>
+</table>
+";
+?>
+
+<h3> 4. Screenshot of the development environment .</h3>
+<img src="images/20230201.Docker.screenshot.png" alt="20230201.Docker.screenshot">
 
 <!--
 </body>
