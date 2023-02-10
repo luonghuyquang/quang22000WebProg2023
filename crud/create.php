06de56c6c5e654800e05c9ccfd330c777359327b
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-10 16:56:20 +0200

    fixing header

---
+++ crud/create.php
@@ -0,0 +1,36 @@
+<?php
+$title = "A simple CRUD app";
+include '../quang22000Layout/header.php';
+?>
+<form method="post" action = "">
+  <input type="text" name="fname" placeholder="First Name" required><br><br>
+  <input type="text" name="lname" placeholder="Last Name" required><br><br>
+  <input type="text" name="city" placeholder="City" required><br><br>
+  <select name="groupid"><br><br>
+    <option value="BBCAP22">BBCAP22</option>
+    <option value="BBCAP21">BBCAP21</option>
+    <option value="Others">Others</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit" name="submit">
+</form>
+<?php
+  if (isset($_POST["submit"])){
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $city = $_POST['city'];
+    $groupid = $_POST['groupid'];
+    include 'db.php';
+    $sql = "insert into studentinfo(fname, lname, city, groupid)
+    values ('$fname', '$lname', '$city', '$groupid')";
+    if ($conn ->query($sql) === true) {
+      echo "Your information is added successfully";
+    }
+    else {
+      echo "Error: ". $conn->error;
+    }
+  }
+?>
+<?php 
+include '../quang22000Layout/footer.php';
+?>
