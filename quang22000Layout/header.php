2d412d2e958aa8f9b7cea91c97bd7acda5122615
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-16 15:59:06 +0200

    relative path

--- quang22000Layout/header.php
+++ quang22000Layout/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous">
     <!--local css-->
-  <link rel="stylesheet" href="/quang22000webprog2023/quang22000layout/style.css">
+  <link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/quang22000layout/style.css'?>">
   <script>
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () { scrollFunction() };
@@ -37,14 +37,14 @@
     <div class="row">
   <nav class="navbar navbar-expand-lg" style="background-color: rgb(65, 7, 109);" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Quang22000</a>
+    <a class="navbar-brand" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/index.php'?>">Quang22000</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" aria-current="page" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/index.php'?>">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="https://docs.google.com/document/d/18lU-MVN4k535k5si3cuPVgixWFlyhNEs/edit" target=blank>inClassTask[word]</a>
@@ -67,19 +67,19 @@
   <div class="row" style="margin-top: 10px; margin-bottom: 10px; margin-right: 5px;">
     <div class="col-sm-3" style="background-color: rgba(228, 235, 194, 0.353);">
       <h4>List of Tasks</h4>
-      <a class="btn btn-success" href="/quang22000webprog2023/qex1.php">31.01.2023 (ex1.php)</a><br><br>
-      <a class="btn btn-success" href="/quang22000webprog2023/index.php">01.02.2023 (index.php)</a><br><br>
-      <a class="btn btn-success" href="/quang22000webprog2023/variableq.php">07.02.2023 (variable.php)</a><br><br>
-      <a class="btn btn-success" href="/quang22000webprog2023/controlflow.php">08.02.2023 (controlflow.php)</a><br><br>
-      <a class="btn btn-success" href="/quang22000webprog2023/array_calculator.php">09.02.2023 (array_calculator.php)</a><br><br>
-      <a class="btn btn-success" href="/quang22000webprog2023/array.php">09.02.2023 (array.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/qex1.php'?>">31.01.2023 (ex1.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/index.php'?>">01.02.2023 (index.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/variableq.php'?>">07.02.2023 (variable.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/controlflow.php'?>">08.02.2023 (controlflow.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/array_calculator.php'?>">09.02.2023 (array_calculator.php)</a><br><br>
+      <a class="btn btn-success" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/array.php'?>">09.02.2023 (array.php)</a><br><br>
       <h4>A simple CRUD app</h4>
-      <a class="btn btn-warning" href="/quang22000webprog2023/crud/create.php">10.02.2023 Create (create.php)</a><br><br>
-      <a class="btn btn-warning" href="/quang22000webprog2023/crud/read.php">14.02.2023 Retrieve (read.php)</a><br><br>
-      <a class="btn btn-warning" href="/quang22000webprog2023/crud/updatesingle.php">14.02.2023 Update or Delete (updatesingle.php)</a><br><br>
+      <a class="btn btn-warning" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/crud/create.php'?>">10.02.2023 Create (create.php)</a><br><br>
+      <a class="btn btn-warning" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/crud/read.php'?>">14.02.2023 Retrieve (read.php)</a><br><br>
+      <a class="btn btn-warning" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/crud/updatesingle.php'?>">14.02.2023 Update or Delete (updatesingle.php)</a><br><br>
       <h4>Team project</h4>
       <a class="btn btn-primary" href="https://github.com/luonghuyquang/team11webprog" target="blank">10.02.2023 (Team 11 project)</a><br><br>
       <h4>Testing area</h4>
-      <a class="btn btn-secondary" href="/quang22000webprog2023/testcode.php">Testing Area</a>
+      <a class="btn btn-secondary" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../quang22000webprog2023/testcode.php'?>">Testing Area</a>
     </div>
     <div class="col-sm-9">
